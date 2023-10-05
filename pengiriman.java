88ded5e91e8a36684a7c140e4e3b9b5d0a828fe2
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2023-10-05 14:19:01 +0700

    penambahan materi dari jobsheet 6

--- pengiriman.java
+++ pengiriman.java
@@ -10,7 +10,6 @@ public class pengiriman {
         String username = scanner.nextLine();
         System.out.print("Masukkan kata sandi: ");
         String password = scanner.nextLine();
-        
 
         if (username.equals("purnama") && password.equals("saya bos")) {
             System.out.println("Login berhasil!");
@@ -22,6 +21,7 @@ public class pengiriman {
             System.out.println("3. Riwayat pembelian");
             System.out.print("Pilihan Anda: ");
             int choice = scanner.nextInt();
+            
 
             switch (choice) {
                 case 1:
@@ -54,31 +54,39 @@ public class pengiriman {
                     tinggi = sc.nextDouble();
                     double biayaPengiriman = beratPaket * 10 + jarakPengiriman * 5 + panjang * 5 + lebar * 5 + tinggi * 5;
 
-                    // Output hasil pengiriman
-                    System.out.println("Nama pengirim: " + namaPengirim);
-                    System.out.println("Nama penerima: " + namaPenerima);
-                    System.out.println("Alamat pengirim: " + alamatPengirim);
-                    System.out.println("Alamat penerima: " + alamatPenerima);
-                    System.out.println("Biaya pengiriman: " + biayaPengiriman);
-
                     //Layanan Pengiriman
                     System.out.println("Pilih layanan pengiriman yang digunakan");
                     System.out.println("1. Reguler");
                     System.out.println("2. Express");
                     System.out.println("Pilih dengan memasukan angka sesuai pilihan");
                     int layanan = scanner.nextInt();
+                    double dimensi = panjang * lebar * tinggi;
+                    double diskon = 0;
+
+                    
                     if (layanan==1){
+                    if (jarakPengiriman > 1000) {
+                        if (beratPaket < 2000) {
+                            if (dimensi < 600) {
+                                diskon = 3*0.05;
+                            } else {
+                                diskon = 2*0.05;
+                            }
+                        } else {
+                            diskon = 0.05;
+                        }
+                    } 
+                    } else {
+                        biayaPengiriman = biayaPengiriman + biayaPengiriman * 50/100;
+                    } 
+                    biayaPengiriman = biayaPengiriman - (biayaPengiriman * diskon);
                     System.out.println("Nama pengirim: " + namaPengirim);
                     System.out.println("Nama penerima: " + namaPenerima);
+                    System.out.println("Alamat pengirim: " + alamatPengirim);
                     System.out.println("Alamat penerima: " + alamatPenerima);
-                    System.out.println("Biaya pengiriman: " + biayaPengiriman);
-                    }  else {
-                    System.out.println("Nama pengirim: " + namaPengirim);
-                    System.out.println("Nama penerima: " + namaPenerima);
-                    System.out.println("Alamat penerima: " + alamatPenerima);
-                    double biayaPengiriman1 = biayaPengiriman + biayaPengiriman * 50/100;
-                    System.out.println("Biaya pengiriman: " + biayaPengiriman1);
-                    }
+                    System.out.println("Biaya pengiriman: " + biayaPengiriman );
+                    System.out.println("Diskon yang di dapat : " + diskon);
+
                     System.out.println("Apakah anda ingin melanjutkan pengiriman? \n 1. Iya \n 2.Tidak");
                     int lanjut = scanner.nextInt();
                     
@@ -93,6 +101,7 @@ public class pengiriman {
                             System.out.println("Keterangan : " +beratPaket+" gram "+jarakPengiriman+" km");
                             System.out.println("Biaya : " + biayaPengiriman);
                             System.out.println("No Resi : RGL01"+jarakPengiriman+beratPaket );
+                            
                         } else {
                             double biayaPengiriman1 = biayaPengiriman + biayaPengiriman * 50/100;
                             System.out.println("Pengirim : " + namaPengirim);
@@ -107,13 +116,10 @@ public class pengiriman {
                         System.out.println("Transaksi gagal");
                     }
                     break;
+                    
                 case 2:
-                    // Pilihan 2: Melacak paket
-                    System.out.println(" Masukkan nomor Resi: ");
-                    String nomorResi = input.nextLine();
-                    System.out.println("Masukkan nama pengirim: " + namaPengirim);
-                    System.out.println("Masukkan nama penerima: " + namaPenerima);
-
+                    
+                  
                 
                     break;
                 case 3:

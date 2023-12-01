43a3c4201a2bef0f8f89de958d5d7a1a86132530
PurnamaRidzkyN <purnamanugraha492@gmail.com>
2023-12-01 23:28:17 +0700

    perbaikan eror dan penambahan level

--- pengiriman.java
+++ pengiriman.java
@@ -1,81 +1,138 @@
 import java.util.Scanner;
 
 public class pengiriman {
-    
-    //Fungsi Riwayat Pengiriman
-    static void tampilRiwayat (String [][] dataPengiriman, double [][] dataPaket, int b ,int k){
-        boolean riwayatDitemukan = false;
-        for (int i = 0; i < dataPengiriman.length; i++) {
-            for (int j = 0; j < dataPengiriman[i].length; j++) {
-        System.out.println("Nama Pengirim    : " + dataPengiriman[b][i]);
-        System.out.println("No Hp Pengirim   : " + dataPengiriman[b + 1][i]);
-        System.out.println("Nama Penerima    : " + dataPengiriman[b + 2][i]);
-        System.out.println("No Hp Penerima   : " + dataPengiriman[b + 3][i]);
-        System.out.println("Alamat Penerima  : " + dataPengiriman[b + 4][i]);
-        System.out.println("No Resi          : " + dataPengiriman[b + 5][i]);
-        System.out.println("Biaya Pengiriman : " + dataPaket[b][i]);
-        System.out.println("Berat Paket      : " + dataPaket[b + 1][i]);
-        System.out.println("Jarak Pengiriman : " + dataPaket[b + 2][i]);
-        riwayatDitemukan = true;
-         break;                     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Login
+        String[][] loginManajer ={
+        {"Manajer","m1anage"}
+        }; 
+        String[][] loginAdmin = {
+        {"admin1", "adm1"},
+        {"admin2", "adm2"}
+        };
+        String[][] loginKurir ={
+        {"Kurir1","kr1"},
+        {"Kurir2","kr1"}
+        };        
+
+        // kumpulan array
+        int maxData = 100;
+        double [][] dataPaket = new double[4][maxData];
+        String [][] dataPengiriman = new String[6][maxData];
+        String  [] tempRiwayat = {"Nama Pengirim       : " ,"No Hp Pengirim      : " ,"Nama Penerima       : " ,"No Hp Penerima      : " ,"Alamat Penerima     : " ,"No Resi             : " ,"Biaya Pengiriman    : " ,"Berat Paket         : " ,"Jarak Pengiriman    : "};
+        while (true) {
+        System.out.println("============================================");
+        System.out.println(" SELAMAT DATANG DI PROGRAM EKSPEDISI DINPUR");        
+        System.out.println("============================================");
+        System.out.println("Masuk sebagai siapa?");
+        System.out.println("1.Manajer");
+        System.out.println("2.Admin");
+        System.out.println("3.Kurir");
+        System.out.println("4.User");
+        System.out.println("5.Ubah bahasa");
+        System.out.println("============================================");
+        System.out.print("Masukan pilihan anda: ");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                manajer(maxData,dataPaket,dataPengiriman,tempRiwayat,loginManajer);
+                break;
+            case 2:
+                admin(maxData,dataPaket,dataPengiriman,tempRiwayat,loginAdmin);
+                break;
+            case 3:
+                kurir(maxData,dataPaket,dataPengiriman,tempRiwayat,loginKurir);
+                break;
+            case 4:
+                user(maxData,dataPaket,dataPengiriman,tempRiwayat);
+                break;
+            default:
+            System.out.println("pilihan anda tidak ada");
+                break;
         }
-        if (riwayatDitemukan) {
-        break;
-        }
-        } if (!riwayatDitemukan) {
-        System.out.println("Riwayat tidak ditemukan.");
-        }
-        System.out.println();
+        
     }
-    //fungsi biaya pengiriman
-    static double biayaPengiriman (double berat, double jarak, double panjang, double lebar, double tinggi, double layanan) {
-        double dimensi, biayaPengiriman, diskon, total;
-        biayaPengiriman = berat * 10 + jarak * 5 + panjang * 5 + lebar * 5 + tinggi * 5;
-        dimensi = panjang *tinggi*lebar;
-        diskon = 0;
-        if (layanan == 1) {
-            if (jarak > 1000) {
-                if (berat < 2000) {
-                    if (dimensi < 600) {
-                        diskon = 3*0.05;
-                    } else {
-                        diskon = 2*0.05;
+    }
+    public static void manajer(int maxData,double[][] dataPaket, String[][] dataPengiriman,String[] tempRiwayat,String[][] loginData){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print("Masukkan nama pengguna (q untuk keluar): ");
+            String user = scanner.nextLine();
+            System.out.print("Masukkan kata sandi (q untuk keluar)   : ");
+            String pass = scanner.nextLine();
+
+            boolean loginBerhasil = false;
+            if(user.equalsIgnoreCase("q")||pass.equalsIgnoreCase("q")){
+                break;
+            }
+            for (int i = 0; i < loginData.length; i++) {
+                String username = loginData [i][0];
+                String password = loginData [i][1];
+
+                if (user.equalsIgnoreCase(username) && pass.equals(password)) {
+                    System.out.println("======================================");
+                    System.out.println("Login berhasil!");
+                    System.out.println("======================================");
+                    loginBerhasil = true;
+                    break;
+                }
+                if (loginBerhasil) {
+                    System.out.println("1. Menampilkan semua riwayat");
+                    System.out.print("Masukan pilihan: ");
+                    int choice = scanner.nextInt();
+                    if (choice==1){
+                        System.out.println("Pilih admin");
+                        System.out.println("1.Admin 1");
+                        System.out.println("2.Admin 2");
+                        System.out.print("Masukan pilihan anda: ");
+                        choice = scanner.nextInt();
+                        
                     }
-                } else {
-                    diskon = 0.05;
                 }
             }
-            total = biayaPengiriman - (biayaPengiriman * diskon);
-
-        } else {
-            total = biayaPengiriman + (biayaPengiriman * (50/100));
-        }
-    return total;
+            }
     }
-    
-    //Fungsi Melacak paket
-    static void melacakPaket (String data, String [][] dataPaket, int b, int k) {
+    public static void kurir(int maxData,double[][] dataPaket, String[][] dataPengiriman,String[] tempRiwayat,String[][] loginData) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.print("Masukkan nama pengguna (q untuk keluar): ");
+            String user = scanner.nextLine();
+            System.out.print("Masukkan kata sandi (q untuk keluar)   : ");
+            String pass = scanner.nextLine();
 
+            boolean loginBerhasil = false;
+            if(user.equalsIgnoreCase("q")||pass.equalsIgnoreCase("q")){
+                break;
+            }
+            for (int i = 0; i < loginData.length; i++) {
+                String username = loginData [i][0];
+                String password = loginData [i][1];
+
+                if (user.equalsIgnoreCase(username) && pass.equals(password)) {
+                    System.out.println("======================================");
+                    System.out.println("Login berhasil!");
+                    System.out.println("======================================");
+                    loginBerhasil = true;
+                    break;
+                }
+                if (loginBerhasil) {
+                    
+                }
+            }
+            }
     }
-
-    public static void main(String[] args) {
+    public static void user(int maxData,double[][] dataPaket, String[][] dataPengiriman,String[] tempRiwayat) {
+        
+    }
+    public static void admin(int maxData,double[][] dataPaket, String[][] dataPengiriman,String[] tempRiwayat,String[][] loginData) {
         Scanner scanner = new Scanner(System.in);
         // inisilisasi untuk riwayat
         int k = 0;
         int b = 0;
         
-        // kumpulan array
-        int maxData = 100;
-        double [][] dataPaket = new double[4][maxData];
-        String [][] dataPengiriman = new String[6][maxData];
-        String  [] tempRiwayat = 
-        {"Nama Pengirim       : " ,"No Hp Pengirim      : " ,"Nama Penerima       : " ,"No Hp Penerima      : " ,"Alamat Penerima     : " ,"No Resi             : " ,"Biaya Pengiriman    : " ,"Berat Paket         : " ,"Jarak Pengiriman    : "};
- 
-        // Login
-        String[][] loginData = {
-        {"admin1", "adm1"},
-        {"admin2", "adm2"}
-    };
+
+    
 
         do {
             System.out.print("Masukkan nama pengguna (q untuk keluar): ");
@@ -275,34 +332,53 @@ public class pengiriman {
                         if (pill == 1) {
                             System.out.print("Masukan nama pengirim : ");
                             String namapengirim = scanner.next();
-                            melacakPaket(namapengirim, dataPaket, b, k);
+                            melacakPaket(namapengirim,dataPengiriman, b, k);
                         } else if (pill == 2) {
                             System.out.print("Masukkan no Hp pengirim :");
                             String nopengirim = scanner.next();
-                            melacakPaket(nopengirim, dataPaket, b, k);
+                            melacakPaket(nopengirim,dataPengiriman, b, k);
                         } else if (pill == 3) {
                             System.out.print("Masukan nama penerima : ");
                             String namapenerima = scanner.next();
-                            melacakPaket(namapenerima, dataPaket, b, k);
+                            melacakPaket(namapenerima,dataPengiriman, b, k);
                         } else if (pill == 4) {
-                            System.out.print("Masukkan no Hp penerima : ");
+                            System.out.print("Masukkan no Hp penerima");
                             String nopenerima = scanner.next();
-                            melacakPaket(nopenerima, dataPaket, b, k);
+                            melacakPaket(nopenerima,dataPengiriman, b, k);
                         } else if (pill == 5) {
                             System.out.print("Masukkan alamat penerima : ");
                             String alamat = scanner.next();
-                            melacakPaket(alamat, dataPaket, b, k);
+                            melacakPaket(alamat,dataPengiriman,  b, k);
                         } else if (pill == 6) {
                             System.out.print("Masukkan no resi : ");
                             String noresi = scanner.next();
-                            melacakPaket(noresi, dataPaket, b, k);
+                            melacakPaket(noresi,dataPengiriman,  b, k);
                         } else {
                             System.out.println(" ======================================");
                             break;
                         }
 
                     } else if (choice == 3) {
-                        while (true) {
+                        pencarianRiwayat(scanner, dataPengiriman, dataPaket, b, k, tempRiwayat);
+                        break;
+                    } else if (choice == 4) {
+                        System.out.println("Anda telah keluar ");
+                        break;
+
+                    } else {
+                        System.out.println("Pilihan tidak valid.");
+                    }
+                }
+
+            
+                
+            } else {
+                System.out.println("Login anda gagal, Nama pengguna atau kata sandi salah!");
+            }
+        } while (true);
+    }
+    static void pencarianRiwayat(Scanner scanner,String[][] dataPengiriman, double[][] dataPaket, int b ,int k,String[] tempRiwayat){
+        while (true) {
                         // ... Bagian untuk riwayat pembelian ...
                         System.out.println(" ======================================");
                         System.out.println("|1.Lewat nama pengirim                 |");
@@ -322,38 +398,38 @@ public class pengiriman {
                             System.out.print("Masukan nama pengirim : ");
                             String namaPengirim = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,namaPengirim, dataPengiriman, dataPaket, b, k);
                             
                         } else if (pil == 2) {
                             System.out.print("Masukan no hp pengirim : ");
                             String nomorPengirim = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,nomorPengirim,dataPengiriman, dataPaket, b, k);
                             
 
                         } else if (pil == 3) {
                             System.out.print("Masukan nama penerima : ");
                             String namaPenerima = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,namaPenerima,dataPengiriman, dataPaket, b, k);
 
                         } else if (pil == 4) {
                             System.out.print("Masukan no hp penerima : ");
                             String nomorPenerima = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,nomorPenerima,dataPengiriman, dataPaket, b, k);
 
                         } else if (pil == 5) {
                             System.out.print("Masukan alamat penerima : ");
                             String alamatPenerima = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,alamatPenerima,dataPengiriman, dataPaket, b, k);
 
                         } else if (pil == 6) {
                             System.out.print("Masukan no resi : ");
                             String noResi = scanner.next();
                             System.out.println("======================================");
-                            tampilRiwayat(dataPengiriman, dataPaket, b, k);
+                            tampilRiwayat(pil,noResi,dataPengiriman, dataPaket, b, k);
                             
 
                         }else if (pil==7){
@@ -379,22 +455,59 @@ public class pengiriman {
                                 break;
                             }
                         }
-
-                    } else if (choice == 4) {
-                        System.out.println("Anda telah keluar ");
-                        break;
-
-                    } else {
-                        System.out.println("Pilihan tidak valid.");
-                    }
-                }
-
-            
-                
-            } else {
-                System.out.println("Login anda gagal, Nama pengguna atau kata sandi salah!");
-            }
-        } while (true);
     }
+    static void tampilRiwayat (int pil,String data, String[][] dataPengiriman, double[][] dataPaket, int b ,int k){
+        boolean riwayatDitemukan = false;
+        for (int i = 0; i < dataPengiriman.length; i++) {
+            for (int j = pil-1; ; j++) { 
+                if (data.equalsIgnoreCase(dataPengiriman[j][i])){
+                System.out.println("Nama Pengirim    : " + dataPengiriman[b][i]);
+                System.out.println("No Hp Pengirim   : " + dataPengiriman[b+1][i]);
+                System.out.println("Nama Penerima    : " + dataPengiriman[b+2][i]);
+                System.out.println("No Hp Penerima   : " + dataPengiriman[b+3][i]);
+                System.out.println("Alamat Penerima  : " + dataPengiriman[b+4][i]);
+                System.out.println("No Resi          : " + dataPengiriman[b+5][i]);                                
+                System.out.println("Biaya Pengiriman : " + dataPaket[b][i]);
+                System.out.println("Berat Paket      : " + dataPaket[b+1][i]);
+                System.out.println("Jarak Pengiriman : " + dataPaket[b+2][i]);
+                riwayatDitemukan = true;                                
+                break;
+                }                            
+        }
+        if (riwayatDitemukan) {
+        break;
+        }
+        } if (!riwayatDitemukan) {
+        System.out.println("Riwayat tidak ditemukan.");
+        }
+    }
+    static double biayaPengiriman (double berat, double jarak, double panjang, double lebar, double tinggi, double layanan) {
+        double dimensi, biayaPengiriman, diskon, total;
+        biayaPengiriman = berat * 10 + jarak * 5 + panjang * 5 + lebar * 5 + tinggi * 5;
+        dimensi = panjang *tinggi*lebar;
+        diskon = 0;
+        if (layanan == 1) {
+            if (jarak > 1000) {
+                if (berat < 2000) {
+                    if (dimensi < 600) {
+                        diskon = 3*0.05;
+                    } else {
+                        diskon = 2*0.05;
+                    }
+                } else {
+                    diskon = 0.05;
+                }
+            }
+            total = biayaPengiriman - (biayaPengiriman * diskon);
+
+        } else {
+            total = biayaPengiriman + (biayaPengiriman * (50/100));
+        }
+    return total;
+    }
+        static void melacakPaket (String data, String [][] dataPaket, int b, int k) {
+
+    }
+
 }
 
